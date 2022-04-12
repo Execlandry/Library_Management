bb36301ff2c5be1835524f2a603c5b44bf804015
vrferns <vrferns17@gmail.com>
2022-04-12 15:39:40 +0530

    edit folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,50 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:library_management/common/app_colors.dart';
-import 'package:library_management/controllers/LocalNavigation/navigation_controller.dart';
-import 'package:library_management/controllers/menu_controller.dart';
-import 'package:library_management/landing.dart';
+//import 'package:get/get.dart';
+//import 'package:google_fonts/google_fonts.dart';
+//import 'package:library_management/common/app_colors.dart';
+//import 'package:library_management/controllers/LocalNavigation/navigation_controller.dart';
+//import 'package:library_management/controllers/menu_controller.dart';
+//import 'package:library_management/landing.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:library_management/pages/dashboard/DisplayData/404/error.dart';
-import 'package:library_management/pages/dashboard/DisplayData/Authentication/auth.dart';
+//import 'package:library_management/pages/dashboard/DisplayData/404/error.dart';
+//import 'package:library_management/pages/dashboard/DisplayData/Authentication/auth.dart';
 
-import 'package:library_management/routes/routes.dart';
+//import 'package:library_management/routes/routes.dart';
 
-void main() {
-  Get.put(MenuController());
-  Get.put(NavigationController());
+//void main() {
+  //Get.put(MenuController());
+  //Get.put(NavigationController());
 
-  runApp(const MyApp());
-}
+  //runApp(const MyApp());
+//}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+//class MyApp extends StatelessWidget {
+  //const MyApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      initialRoute: AuthenticationPageRoute,
-      unknownRoute: GetPage(
-          name: "/not-found",
-          page: () => PageNotFound(),
-         transition: Transition.fadeIn),
-      getPages: AppRoutes.routes,
+//  @override
+  //Widget build(BuildContext context) {
+    //return GetMaterialApp(
+      //initialRoute: AuthenticationPageRoute,
+     // unknownRoute: GetPage(
+       //   name: "/not-found",
+         // page: () => PageNotFound(),
+         //transition: Transition.fadeIn),
+    //  getPages: AppRoutes.routes,
 
-      title: 'LibMe',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          scaffoldBackgroundColor: AppColor.light,
-          textTheme: GoogleFonts.mulishTextTheme(Theme.of(context).textTheme)
-              .apply(bodyColor: Colors.black),
-           pageTransitionsTheme: const PageTransitionsTheme(builders: {
-           TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
-           TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
-          }),
-         primaryColor: Colors.blue),
+      //title: 'LibMe',
+      //debugShowCheckedModeBanner: false,
+      //theme: ThemeData(
+        //  scaffoldBackgroundColor: AppColor.light,
+          //textTheme: GoogleFonts.mulishTextTheme(Theme.of(context).textTheme)
+             // .apply(bodyColor: Colors.black),
+           //pageTransitionsTheme: const PageTransitionsTheme(builders: {
+           //TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
+          // TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+          //}),
+        // primaryColor: Colors.blue),
+      //   {
       // initialRoute: AppRoutes.dashboard,
       // getPages: AppRoutes.routes,
       // home: AnimatedSplashScreen(
@@ -52,8 +53,8 @@ class MyApp extends StatelessWidget {
       //     splashTransition: SplashTransition.scaleTransition,
       //     backgroundColor: Color.fromARGB(255, 126, 191, 245),
       //     nextScreen: Landing()),
-    );
-  }
-}
-
+    //);
+  //}
+//}
+//}
 
---
+++ lib/widgets/Animation/anime.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class AnimePage extends StatefulWidget{
+  const AnimePage ({Key? key}) : super(key: key);
+
+  @override 
+  _AnimePageState createState() =>_AnimePageState(); 
+}
+
+class _AnimePageState extends State<AnimePage> with SingleTickerProviderStateMixin{
+   //controller
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+      );
+
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _controller.dispose();
+  }
+
+  bool  bookmarked = false;
+
+
+  @override
+  Widget build(BuildContext context){
+    
+    return Scaffold(
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            if (bookmarked==false){
+              bookmarked=true;
+              _controller.forward();
+            }
+            else{
+              bookmarked=false;
+              _controller.reverse();
+            }
+          },
+          child: Lottie.network(
+             'https://assets9.lottiefiles.com/packages/lf20_fz6xe55o.json',
+              controller: _controller,
+        ),     
+       ),
+      )
+    );
+  }
+}
