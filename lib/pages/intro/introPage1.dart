24b603b7d13445709ab2c92f067dc178f005afad
vrferns <vrferns17@gmail.com>
2022-04-12 15:29:06 +0530

    editing folders

---
+++ lib/pages/intro/introPage1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class IntroPage1 extends StatelessWidget {
+
+  @override 
+  Widget build(BuildContext context){
+    return Container(
+      color: Colors.pinkAccent,
+      child: Center(
+        child:Text('Page 1'),
+       ),
+    );
+  }
+}
